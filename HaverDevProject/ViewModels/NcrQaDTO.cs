b48e29c2d70c6413a27d95fd4ceccbf76d743878
Victor Lopez <lopezchavezvictorh@gmail.com>
2024-02-16 22:34:12 -0500

    NCR Number autogenerated

---
+++ HaverDevProject/ViewModels/NcrQaDTO.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using HaverDevProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HaverDevProject.ViewModels
+{    
+    public class NcrQaDTO
+    {
+
+        //public int NcrId { get; set; }
+        public string NcrNumber { get; set; }
+
+        //public DateTime NcrLastUpdated { get; set; }
+
+        public bool NcrStatus { get; set; } = true;        
+        public bool NcrQaItemMarNonConforming { get; set; }        
+        public bool NcrQaProcessApplicable { get; set; }        
+        public DateTime NcrQacreationDate { get; set; }       
+        public string NcrQaOrderNumber { get; set; }        
+        public string NcrQaSalesOrder { get; set; }        
+        public int NcrQaQuanReceived { get; set; }        
+        public int NcrQaQuanDefective { get; set; }       
+        public string NcrQaDescriptionOfDefect { get; set; }
+        public int NcrId { get; set; }
+        public int ItemId { get; set; }
+        public bool NcrQaEngDispositionRequired { get; set; }
+
+    }
+}
