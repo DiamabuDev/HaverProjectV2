b4fdc0a2944dbe9792e17d5a93813234f03fe257
Nigel Temple <you@example.com>
2024-02-26 20:24:12 -0500

    Added functionality to index and details page for ReInspect

--- HaverDevProject/Controllers/NcrReInspectController.cs
+++ HaverDevProject/Controllers/NcrReInspectController.cs
@@ -10,6 +10,8 @@ using HaverDevProject.Models;
 using Microsoft.EntityFrameworkCore.Storage;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using HaverDevProject.CustomControllers;
+using HaverDevProject.Utilities;
+using HaverDevProject.ViewModels;
 
 namespace HaverDevProject.Controllers
 {
@@ -23,10 +25,150 @@ namespace HaverDevProject.Controllers
         }
 
         // GET: NcrReInspect
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string SearchCode, int? page, int? pageSizeID, string actionButton,
+            DateTime StartDate, DateTime EndDate, string sortDirection = "desc", string sortField = "Created", string filter = "Active")
         {
-            var haverNiagaraContext = _context.NcrReInspects.Include(n => n.Ncr);
-            return View(await haverNiagaraContext.ToListAsync());
+            //Set the date range filer based on the values in the database
+            if (EndDate == DateTime.MinValue)
+            {
+                StartDate = _context.NcrQas
+                .Min(f => f.NcrQacreationDate.Date);
+
+                EndDate = _context.NcrQas
+                .Max(f => f.NcrQacreationDate.Date);
+
+                ViewData["StartDate"] = StartDate.ToString("yyyy-MM-dd");
+                ViewData["EndDate"] = EndDate.ToString("yyyy-MM-dd");
+            }
+            //Check the order of the dates and swap them if required
+            if (EndDate < StartDate)
+            {
+                DateTime temp = EndDate;
+                EndDate = StartDate;
+                StartDate = temp;
+            }
+
+            string[] sortOptions = new[] { "Created", "Acceptable", "Inspected By", "NCR #" };
+
+            var ncrReInspect = _context.NcrReInspects
+                .Include(n => n.Ncr)
+                .Include(n => n.Ncr).ThenInclude(n => n.NcrQa)
+                .AsNoTracking();
+
+            //Filterig values            
+            if (!System.String.IsNullOrEmpty(filter))
+            {
+                if (filter == "Active")
+                {
+                    ncrReInspect = ncrReInspect.Where(n => n.Ncr.NcrStatus == true);
+                }
+                else //(filter == "Closed")
+                {
+
+                    ncrReInspect = ncrReInspect.Where(n => n.Ncr.NcrStatus == false);
+                }
+            }
+
+            if (!System.String.IsNullOrEmpty(SearchCode))
+            {
+                ncrReInspect = ncrReInspect.Where(s => s.Ncr.NcrNumber.ToUpper().Contains(SearchCode.ToUpper()));
+            }
+            if (StartDate == EndDate)
+            {
+                ncrReInspect = ncrReInspect.Where(n => n.Ncr.NcrQa.NcrQacreationDate == StartDate);
+            }
+            else
+            {
+                ncrReInspect = ncrReInspect.Where(n => n.Ncr.NcrQa.NcrQacreationDate >= StartDate &&
+                         n.Ncr.NcrQa.NcrQacreationDate <= EndDate);
+            }
+
+            //Sorting columns
+            if (!System.String.IsNullOrEmpty(actionButton)) //Form Submitted!
+            {
+                page = 1;//Reset page to start
+
+                if (sortOptions.Contains(actionButton))//Change of sort is requested
+                {
+                    if (actionButton == sortField) //Reverse order on same field
+                    {
+                        sortDirection = sortDirection == "asc" ? "desc" : "asc";
+                    }
+                    sortField = actionButton;//Sort by the button clicked
+                }
+            }
+
+            if (sortField == "Created")
+            {
+                if (sortDirection == "desc") //desc by default
+                {
+                    ncrReInspect = ncrReInspect
+                        .OrderBy(p => p.Ncr.NcrQa.NcrQacreationDate);
+
+                    ViewData["filterApplied:Created"] = "<i class='bi bi-sort-up'></i>";
+                }
+                else
+                {
+                    ncrReInspect = ncrReInspect
+                        .OrderByDescending(p => p.Ncr.NcrQa.NcrQacreationDate);
+
+                    ViewData["filterApplied:Created"] = "<i class='bi bi-sort-down'></i>";
+                }
+            }
+            else if (sortField == "Acceptable")
+            {
+                if (sortDirection == "asc")
+                {
+                    ncrReInspect = ncrReInspect
+                        .OrderBy(p => p.NcrReInspectAcceptable);
+                    ViewData["filterApplied:NcrReInspectAcceptable"] = "<i class='bi bi-sort-up'></i>";
+                }
+                else
+                {
+                    ncrReInspect = ncrReInspect
+                        .OrderByDescending(p => p.NcrReInspectAcceptable);
+                    ViewData["filterApplied:NcrReInspectAcceptable"] = "<i class='bi bi-sort-down'></i>";
+                }
+            }
+            else if (sortField == "Inspected By")
+            {
+                if (sortDirection == "asc")
+                {
+                    ncrReInspect = ncrReInspect
+                        .OrderBy(p => p.NcrReInspectUserId);
+                    ViewData["filterApplied:NcrReInspectUserId"] = "<i class='bi bi-sort-up'></i>";
+                }
+                else
+                {
+                    ncrReInspect = ncrReInspect
+                        .OrderByDescending(p => p.NcrReInspectUserId);
+                    ViewData["filterApplied:NcrReInspectUserId"] = "<i class='bi bi-sort-down'></i>";
+                }
+            }
+            else
+            {
+                if (sortDirection == "asc")
+                {
+                    ncrReInspect = ncrReInspect
+                        .OrderBy(p => p.Ncr.NcrNumber);
+                    ViewData["filterApplied:Ncr"] = "<i class='bi bi-sort-up'></i>";
+                }
+                else
+                {
+                    ncrReInspect = ncrReInspect
+                        .OrderByDescending(p => p.Ncr.NcrNumber);
+                    ViewData["filterApplied:Ncr"] = "<i class='bi bi-sort-down'></i>";
+                }
+            }
+
+            ViewData["sortField"] = sortField;
+            ViewData["sortDirection"] = sortDirection;
+
+            int pageSize = PageSizeHelper.SetPageSize(HttpContext, pageSizeID, ControllerName());
+            ViewData["pageSizeID"] = PageSizeHelper.PageSizeList(pageSize);
+            var pagedData = await PaginatedList<NcrReInspect>.CreateAsync(ncrReInspect.AsNoTracking(), page ?? 1, pageSize);
+
+            return View(pagedData);
         }
 
         // GET: NcrReInspect/Details/5
@@ -39,6 +181,7 @@ namespace HaverDevProject.Controllers
 
             var ncrReInspect = await _context.NcrReInspects
                 .Include(n => n.Ncr)
+                .Include(n => n.Ncr).ThenInclude(n => n.NcrQa)
                 .FirstOrDefaultAsync(m => m.NcrReInspectId == id);
             if (ncrReInspect == null)
             {
@@ -123,6 +266,15 @@ namespace HaverDevProject.Controllers
                 try
                 {
                     await _context.SaveChangesAsync();
+
+                    var ncrToUpdate = await _context.Ncrs.FindAsync(ncrReInspectToUpdate.NcrId);
+                    if (ncrToUpdate != null)
+                    {
+                        ncrToUpdate.NcrStatus = false;
+                        _context.Entry(ncrToUpdate).State = EntityState.Modified;
+                        await _context.SaveChangesAsync();
+                    }
+
                     return RedirectToAction(nameof(Index));
                 }
                 catch (DbUpdateConcurrencyException)
@@ -211,6 +363,36 @@ namespace HaverDevProject.Controllers
             }
 
             return View(ncrReInspect);
+        }
+
+        private async Task AddReInspectPictures(NcrReInspectDTO ncrReInspectDTO, List<IFormFile> pictures)
+        {
+            if (pictures != null && pictures.Any())
+            {
+                ncrReInspectDTO.NcrReInspectPhotos = new List<NcrReInspectPhoto>();
+
+                foreach (var picture in pictures)
+                {
+                    string mimeType = picture.ContentType;
+                    long fileLength = picture.Length;
+
+                    if (!(mimeType == "" || fileLength == 0))
+                    {
+                        if (mimeType.Contains("image"))
+                        {
+                            using var memoryStream = new MemoryStream();
+                            await picture.CopyToAsync(memoryStream);
+                            var pictureArray = memoryStream.ToArray();
+
+                            ncrReInspectDTO.NcrReInspectPhotos.Add(new NcrReInspectPhoto
+                            {
+                                NcrReInspectPhotoContent = ResizeImage.shrinkImageWebp(pictureArray, 500, 600),
+                                NcrReInspectPhotoMimeType = "image/webp"
+                            });
+                        }
+                    }
+                }
+            }
         }
 
         private bool NcrReInspectExists(int id)
--- HaverDevProject/Data/HaverNiagaraContext.cs
+++ HaverDevProject/Data/HaverNiagaraContext.cs
@@ -72,6 +72,9 @@ public partial class HaverNiagaraContext : DbContext
 
     public virtual DbSet<NcrProcurement> NcrProcurements { get; set; }
 
+    public virtual DbSet<NcrReInspectPhoto> NcrReInspectPhotos { get; set; }
+
+
     //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     //    => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=HaverNiagara;Trusted_Connection=SSPI;encrypt=false;");
 
@@ -165,7 +168,15 @@ public partial class HaverNiagaraContext : DbContext
         {
             entity.HasKey(e => e.OpDispositionTypeId).HasName("pk_opDispositionType_opDispositionTypeId");
         });
-               
+
+        modelBuilder.Entity<NcrReInspectPhoto>(entity =>
+        {
+            entity.HasKey(e => e.NcrReInspectPhotoId).HasName("pk_ncrReInspectPhoto_ncrReInspectPhotoId");
+
+            entity.HasOne(d => d.NcrReInspect).WithMany(p => p.NcrReInspectPhotos)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+        });
+
 
         modelBuilder.Entity<Item>()
             .HasIndex(i => i.ItemNumber)
