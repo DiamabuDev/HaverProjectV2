3e681206bcfe265c2a4b8a73de895c73dd045d69
jorge1180 <80490272+jorge1180@users.noreply.github.com>
2024-03-05 23:56:49 -0500

    Details Functionality in pendings

--- HaverDevProject/wwwroot/js/NcrEngJS.js
+++ HaverDevProject/wwwroot/js/NcrEngJS.js
@@ -4,21 +4,22 @@
 
 function GetNcrs() {
     $.ajax({
-        url: '/NcrOperation/GetNcrs',
+        url: '/' + section + '/GetNcrs',
         type: 'GET',
         dataType: 'json',
         contentType: 'application/json;charset=utf-8',
         success: function (response) {
             if (response.length === 0) {
-                $('#tblBody').html('<tr><td colspan="3">No New NCRs available</td></tr>');
+                $('#tblBody').html('<tr><td colspan="4">No New NCRS available</td></tr>');
             } else {
                 $('#tblBody').empty(); // Clear existing rows
                 for (let i = 0; i < response.length; i++) {
-                    const ncrEng = response[i];
+                    const ncr = response[i];
                     const $row = $('<tr>');
-                    $row.append(`<td>${ncrEng.ncrNumber}</td>`);
-                    $row.append(`<td>${ncrEng.supplierName}</td>`);//Supplier
-                    $row.append(`<td><button class="btn btn-success" onclick="startNcrOperation('${ncrEng.ncrNumber}')"><i class="bi bi-play-fill"></i> Start</button></td>`);
+                    $row.append(`<td>${ncr.ncrNumber}</td>`);
+                    $row.append(`<td>${ncr.supplierName}</td>`);//Supplier
+                    $row.append(`<td><button class="btn btn-info" onclick="viewDetails('${ncr.ncrId}')"><i class="bi bi-info-square"></i> Details</button></td>`);
+                    $row.append(`<td><button class="btn btn-success" onclick="start('${ncr.ncrNumber}')"><i class="bi bi-play-fill"></i> Start</button></td>`);
                     $('#tblBody').append($row);
                 }
             }
@@ -29,6 +30,10 @@ function GetNcrs() {
     });
 }
 
-function startNcrOperation(ncrNumber) {
-    window.location.href = '/NcrOperation/Create?ncrNumber=' + ncrNumber;
+function start(ncrNumber) {
+    window.location.href = '/' + section + '/Create?ncrNumber=' + ncrNumber;
+}
+
+function viewDetails(ncrId) {
+    window.location.href = '/' + 'NcrEng' + '/Details/' + ncrId;
 }
